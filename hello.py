6fb41b2268d6630348bbefb5e4f37446e16bbdaf
meetparekh-cbi <meet.parekh@cbinsights.com>
2020-06-16 01:08:51 -0400

    slight change

--- hello.py
+++ hello.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "Hello World"
+    return "Hello World, Now I Use CI/CD"
 
 
 if __name__ == "__main__":
--- test_hello.py
+++ test_hello.py
@@ -2,6 +2,6 @@ from hello import hello_world
 
 
 def test_hello_world():
-    expected = "Hello World"
+    expected = "Hello World, Now I Use CI/CD"
     result = hello_world()
     assert expected == result
