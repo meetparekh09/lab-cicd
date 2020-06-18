f8c172b2745263a6dc81db9b63d02c64bedabc12
meetparekh-cbi <meet.parekh@cbinsights.com>
2020-06-17 23:47:58 -0400

    added database connection added secrets to task added db test endpoint

--- Makefile
+++ Makefile
@@ -2,7 +2,9 @@ build:
 	docker build -t flask-basic .
 
 run:
-	docker run -dp 5000:5000 -v ${PWD}:/lab-cicd flask-basic python hello.py
+	docker run -dp 5000:5000 -e MYSQL_TEST_HOST -e MYSQL_TEST_USER \
+	-e MYSQL_TEST_PASSWORD -e MYSQL_TEST_DATABASE \
+	-v ${PWD}:/lab-cicd flask-basic python hello.py
 
 unit:
 	docker run -v ${PWD}:/lab-cicd flask-basic pytest .
--- hello.py
+++ hello.py
@@ -1,4 +1,13 @@
 from flask import Flask
+import pymysql
+import os
+
+host = os.environ["MYSQL_TEST_HOST"]
+user = os.environ["MYSQL_TEST_USER"]
+password = os.environ["MYSQL_TEST_PASSWORD"]
+database = os.environ.get("MYSQL_TEST_DATABASE", "mysql")
+
+db = pymysql.connect(host, user, password, database)
 
 app = Flask(__name__)
 
@@ -6,6 +15,19 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return "Hello World"
+
+
+@app.route("/test_db/")
+def show_db():
+    cursor = db.cursor()
+    cursor.execute("select * from db")
+    db_result = cursor.fetchall()
+
+    result = []
+    for row in db_result:
+        result.extend(list(row))
+
+    return ",".join(result)
 
 
 if __name__ == "__main__":
