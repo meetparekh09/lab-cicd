09b77f92f6584102d04421a388e570ce14403f03
meetparekh-cbi <meet.parekh@cbinsights.com>
2020-07-13 00:53:26 -0400

    removed database

--- hello.py
+++ hello.py
@@ -1,15 +1,5 @@
 from flask import Flask
-import pymysql
 import os
-
-host = os.environ.get("MYSQL_TEST_HOST", "")
-user = os.environ.get("MYSQL_TEST_USER", "")
-password = os.environ.get("MYSQL_TEST_PASSWORD", "")
-database = os.environ.get("MYSQL_TEST_DATABASE", "mysql")
-
-db = None
-if host != "":
-    db = pymysql.connect(host, user, password, database)
 
 app = Flask(__name__)
 
@@ -17,19 +7,6 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return "Hello World"
-
-
-@app.route("/test_db/")
-def show_db():
-    cursor = db.cursor()
-    cursor.execute("select * from db")
-    db_result = cursor.fetchall()
-
-    result = []
-    for row in db_result:
-        result.extend(list(row))
-
-    return ",".join(result)
 
 
 if __name__ == "__main__":
